298cc8b478e14a8ff21e926ab07f7ae2a436cf44
riqalves <henrique12095@gmail.com>
2025-05-26 09:41:20 -0300

    allow cors

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:8081",
     "http://localhost:5173",
     "https://campus-face.vercel.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
