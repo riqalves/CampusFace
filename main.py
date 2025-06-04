8ca5049eb15d5b5016b91637abbdbf51cac7e1de
riqalves <henrique12095@gmail.com>
2025-06-04 16:54:19 -0300

    feat: requestController

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from router.userRouter import user_router
-from router.authRouter import auth_router
 from router.hubRouter import hub_router
-from router.imageRouter import image_router
+from router.requestRouter import request_router
 from router.faceRouter import face_router
+from router.imageRouter import image_router
+from router.authRouter import auth_router
 
 app = FastAPI()
 
@@ -43,6 +44,7 @@ app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(hub_router, prefix="/hub", tags=["Hub"])
 app.include_router(image_router, prefix="/image", tags=["Image"])
 app.include_router(face_router, prefix="/face", tags=["Face"])
+app.include_router(request_router, prefix="/request", tags=["Request"])
 
 
 
